aa5b96c179f9b1fcc0854b347bed909dfa415bde
unknown <dollorinfotech@gmail.com>
2022-12-05 09:04:16 +0530

    commmit1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Single Line comments
+
+/*
+   Multi Line comment
+
+   Lets take a JS Object for storing student informatino
+*/
+
+
+var student1_name = "sampat";
+var student1_surname = "Prajapati";
+
+var student1 = {
+    //1 Property:Value
+    name:"sampat",
+    surname:"prajapati"
+
+    //2. 
+};// JS 
+
+
+//How many way i can display the output of a JS Code
+
+//1 Way of To see the js code output
+
+// object.propertyname
+
+console.log("Hello "+student1.name); // + is a concatination operator
---
+++ js/script2.js
@@ -0,0 +1,26 @@
+
+ 
+// Var  1995 - 2022
+
+// Let  2015 ES6  
+
+
+// Const  2016 ES6
+
+const student4 = {
+    //1. Property:Value
+    name:"suryabhan", //String
+    surname:'Sisodiya', //String
+    father_name:`Shree Satyam ji Sisodiya` //String
+
+
+
+    //2. 
+}; //JS object
+
+//How many way i can see the JS Output
+
+// 4th Way  document.write()
+
+//object.propertyname
+document.write('<div>'+student4.name+'</div>');
